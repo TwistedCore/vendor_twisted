cceea7eb129a9e6df7d269d80777524e8a9d317a
aow1980 <wilkes.allen@gmail.com>
2015-10-12 19:42:21 -0400

    add userdebugs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,10 @@
 #add_lunch_combo twisted_angler-user
 #add_lunch_combo twisted_bullhead-user
 add_lunch_combo twisted_flo-user
+add_lunch_combo twisted_flo-userdebug
 add_lunch_combo twisted_hammerhead-user
+add_lunch_combo twisted_hammerhead-userdebug
 #add_lunch_combo twisted_mako-user
 add_lunch_combo twisted_shamu-user
+add_lunch_combo twisted_shamu-userdebug
 
