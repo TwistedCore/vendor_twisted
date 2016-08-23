6cb394b00951d06b63659794d8c8173e1550fc5f
aow1980 <wilkes.allen@gmail.com>
2016-08-23 12:31:47 -0400

    remove non arm devices from vendorsetup (reverted from commit fb145df5182fa5838aeba04d0378fa5a8b689b96)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,5 +1,9 @@
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twisted_angler.mk \
+    $(LOCAL_DIR)/twisted_bullhead.mk \
+    $(LOCAL_DIR)/twisted_flo.mk \
     $(LOCAL_DIR)/twisted_hammerhead.mk \
+    $(LOCAL_DIR)/twisted_mako.mk \
     $(LOCAL_DIR)/twisted_shamu.mk \
 
 
