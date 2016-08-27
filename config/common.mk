e0ce6d363d6a132fc46a9bfafd6fff35d93d27dd
aow1980 <wilkes.allen@gmail.com>
2016-08-26 21:31:56 -0400

    let's remove all this and get a cleaner start for N

--- config/common.mk
+++ config/common.mk
@@ -1,122 +1,64 @@
-# Brand
-PRODUCT_BRAND ?= twisted
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-# Local path for prebuilts
-LOCAL_PATH:= vendor/twisted/prebuilts/common/system
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false \
-    drm.service.enabled=true \
-    net.tethering.noprovisioning=true \
-    persist.sys.dun.override=0 \
-    ro.setupwizard.network_required=false \
-    ro.setupwizard.gservices_delay=-1 \
-    ro.build.selinux=1 \
-    ro.adb.secure=1 \
-    ro.substratum.recreate=true
+    ro.com.android.dataroaming=false
 
-# Nova Launcher
-#PRODUCT_COPY_FILES += \
-#    vendor/twisted/prebuilt/common/app/Nova.apk:system/app/Nova/Nova.apk \
-#    vendor/twisted/prebuilt/common/lib/libgif.so:system/app/Nova/lib/arm/libgif.so
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
 
-# Common overlay
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/twisted/overlay/common
+# Disable excessive dalvik debug messages
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.debug.alloc=0
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Build SimToolKit
+# Optional packages
 PRODUCT_PACKAGES += \
-    Stk
+    Basic \
+    LiveWallpapersPicker \
+    PhaseBeam
 
-# Camera Effects
-ifneq ($(filter twisted_flounder twisted_hammerhead twisted_shamu,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES +=  \
-    vendor/twisted/prebuilt/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
-    vendor/twisted/prebuilt/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
-endif
-
-# Latin IME lib - gesture typing
-PRODUCT_COPY_FILES += \
-    vendor/twisted/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
-
-# Extra packages
+# Extra Optional packages
 PRODUCT_PACKAGES += \
-    AdAway \
-    Browser \
-    Busybox \
-    Gallery2 \
-    KernelAdiutor \
-    Launcher3 \
-    LMT \
-    Terminal \
-    WallpaperPicker \
-    masquerade
 
-## Extra overlay packages for day night theming
-include vendor/twisted/overlays/Android.mk
-
-PRODUCT_PACKAGES += \
-    GooglePackageInstallerOverlay
-
-# APN list
-PRODUCT_COPY_FILES += \
-    vendor/twisted/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# init.d script support
-PRODUCT_COPY_FILES += \
-    vendor/twisted/prebuilt/bin/sysinit:system/bin/sysinit
-    
-# twisted-specific init file
-PRODUCT_COPY_FILES += \
-    vendor/twisted/prebuilt/bin/init.twisted.rc:root/init.twisted.rc
-    
-# Backup Tool
-PRODUCT_COPY_FILES += \
-    vendor/twisted/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/twisted/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/twisted/prebuilt/bin/50-base.sh:system/addon.d/50-base.sh \
-
-# Bootanimation support
-PRODUCT_COPY_FILES += \
-    vendor/twisted/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/twisted/overlay/common \
+    vendor/twisted/overlay/dictionaries
 
 # Versioning System
-PRODUCT_VERSION_MAJOR = 6.0.1
-PRODUCT_VERSION_MINOR = BUILD
-PRODUCT_VERSION_MAINTENANCE = 5.0
-ifdef TWISTED_BUILD_EXTRA
-    TWISTED_POSTFIX := -$(TWISTED_BUILD_EXTRA)
-endif
-ifndef TWISTED_BUILD_TYPE
-    TWISTED_BUILD_TYPE := build
-    TWISTED_POSTFIX := $(shell date +"%Y%m%d")
-endif
+PRODUCT_VERSION_MAJOR = 7
+PRODUCT_VERSION_MINOR = 0.1
+PRODUCT_VERSION_MAINTENANCE = 0.1
+TWISTED_BUILD_TYPE =Alpha
+
+TWISTED_POSTFIX := $(shell date +"%Y%m%d-%H%M")
+TWISTED_DEVICE_SHORT := $(TWISTED_DEVICE)
+TWISTED_DEVICE_SHORT := $(subst twisted_,,$(TWISTED_DEVICE_SHORT))
 
 # Set all versions
-TWISTED_VERSION := Twisted-$(TWISTED_BUILD)-$(PRODUCT_VERSION_MAJOR)-$(PRODUCT_VERSION_MINOR)-$(PRODUCT_VERSION_MAINTENANCE)
-TWISTED_MOD_VERSION := Twisted-$(TWISTED_BUILD)-$(PRODUCT_VERSION_MAJOR)-$(TWISTED_BUILD_TYPE)-$(PRODUCT_VERSION_MAINTENANCE)-$(TWISTED_POSTFIX)
+TWISTED_VERSION := $(TARGET_PRODUCT)-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(TWISTED_BUILD_TYPE)-$(PRODUCT_VERSION_MAINTENANCE)-$(TWISTED_POSTFIX)
+TWISTED_MOD_VERSION := screwd-$(TWISTED_DEVICE_SHORT)-$(SCREWD_VERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
-    twisted.ota.version=$(PRODUCT_VERSION_MAJOR)-$(PRODUCT_VERSION_MINOR)-$(PRODUCT_VERSION_MAINTENANCE) \
+    ro.ota.version=$(PRODUCT_VERSION_MAJOR)-$(TWISTED_POSTFIX) \
     ro.twisted.version=$(TWISTED_VERSION) \
     ro.modversion=$(TWISTED_MOD_VERSION) \
-    ro.twisted.buildtype=$(TWISTED_BUILD_TYPE) \
-    twisted.build.version=$(PRODUCT_VERSION_MINOR)-$(PRODUCT_VERSION_MAINTENANCE)
-
-FINISHER_SCRIPT := vendor/twisted/tools/finisher
-SQUISHER_SCRIPT := vendor/twisted/tools/squisher
-CHANGELOG_SCRIPT := vendor/twisted/tools/changelog.sh
+    ro.twisted.buildtype=$(TWISTED_BUILD_TYPE)
 
